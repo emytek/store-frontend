028d5176768d85c93607c3c14c31a261a3b3b674
emytek <66952283+emytek@users.noreply.github.com>
2023-01-26 08:17:59 +0100

    url

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "https://simple-product-store.fly.dev/",
 });
 
 export default instance;
